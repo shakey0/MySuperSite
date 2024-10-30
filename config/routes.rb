1062f1a665a509e71f0c4c98021db5b9da2fa234
shakey0 <shakeyshakespeare0@gmail.com>
2024-10-30 11:33:37 +0000

    fix route helpers

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get "cats/" => "cats#index"
-  get "cats/:slug" => "cats#show"
-  get "m/:id" => "m#show"
-  get "/" => "home#index"
+  get 'cats/index', to: 'cats#index', as: 'cats_index'
+  get 'cats/show', to: 'cats#show', as: 'cats_show'
+  get 'm/show', to: 'm#show', as: 'm_show'
+  get 'home/index', to: 'home#index', as: 'home_index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
