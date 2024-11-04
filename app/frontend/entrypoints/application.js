87a0be1afea2f90040375a8341e322e52eeeb284
shakey0 <shakeyshakespeare0@gmail.com>
2024-11-04 00:11:21 +0000

    SCSS added

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -1,3 +1,3 @@
 // app/frontend/entrypoints/application.js
 // Import any global dependencies here, like Rails UJS or Turbo
-import "../stylesheets/application.css"; // If you have stylesheets
+import "../stylesheets/application.scss"; // If you have stylesheets
