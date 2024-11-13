7ea84ea766def30f9a7cc5a4ad3ff5b990f906fb
shakey0 <shakeyshakespeare0@gmail.com>
2024-11-13 11:51:40 +0000

    Messages functionality largely done

--- app/controllers/m_controller.rb
+++ app/controllers/m_controller.rb
@@ -4,10 +4,21 @@ class MController < ApplicationController
 
   def m_data
     secret = params[:secret]
-    if secret == "koala888"
-      render json: { message: "success" }
+    user_data = load_user_data(secret)
+    if user_data.present?
+      render json: { status: "success", data: user_data }
     else
-      render json: { message: "failure" }
+      render json: { status: "failed" }
+    end
+  end
+
+  def load_user_data(secret)
+    file_path = Rails.root.join("persistent_disk", "m", "#{secret}.json")
+    if File.exist?(file_path)
+      file_content = File.read(file_path)
+      user_data = JSON.parse(file_content)
+    else
+      user_data = {}
     end
   end
 end
--- app/frontend/components/M.jsx
+++ app/frontend/components/M.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 
 export default function Home() {
-  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
   const [secret, setSecret] = useState('');
 
   useEffect(() => {
@@ -13,19 +13,44 @@ export default function Home() {
       try {
         const response = await fetch(`/m_data?secret=${secretFromURL}`);
         const data = await response.json();
-        setMessage(data.message);
+        if (data.status === 'success') {
+          let currentConvo = null;
+          for (let i = 0; i < data.data.convos.length; i++) {
+            // If the convo start_time is up to an hour ago
+            const convoStartTime = Date.parse(data.data.convos[i].start_time.replace(" ", "T"));
+            if (convoStartTime > Date.now() - 3600000) {
+              currentConvo = data.data.convos[i].messages;
+              break;
+            }
+          }
+          if (currentConvo !== null) {
+            setMessages(currentConvo);
+          } else {
+            setMessages([{ direction: 'none', message: 'No messages in the last hour' }]);
+          }
+        } else {
+          setMessages([{ direction: 'invalid', message: "That's not a secret code." }]);
+        }
       } catch (error) {
         console.error('Error fetching data:', error);
-        setMessage('Error loading message');
+        setMessages([{ direction: 'error', message: "Sorry, but something didn't go right there. I'll be looking into this." }]);
       }
     };
 
-    // Fetch data every second
-    const intervalId = setInterval(fetchData, 1000);
+    fetchData();
+    const intervalId = setInterval(fetchData, 2000);
 
-    // Cleanup interval on component unmount
     return () => clearInterval(intervalId);
   }, []);
 
-  return <div>{message}</div>;
+  return (
+    <div>
+      <h1>Messages for secret: {secret}</h1>
+      <ul>
+        {messages.map(({ message, direction }, index) => (
+          <li key={index}>{message}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
