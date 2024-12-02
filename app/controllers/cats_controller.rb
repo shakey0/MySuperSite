16a58ed4a088001999d2e3e7a7d37094e9d60d72
shakey0 <shakeyshakespeare0@gmail.com>
2024-12-02 10:44:28 +0000

    Attempted security fix 2

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -15,25 +15,63 @@ class CatsController < ApplicationController
     slug = params[:slug] || "default"
     filename = params[:filename] || "default.jpg"
 
-    unless slug.match?(/\A[a-z_]+\z/) && filename.match?(/\A[a-zA-Z0-9_-]+\.(jpg|webp)\z/)
+    unless slug.match?(/\A[a-z_]+\z/) && filename.match?(/\A[a-zA-Z0-9_-]+\.(jpg|webp)\z/) # REMOVE jpg LATER
       render plain: "Invalid slug or filename", status: :bad_request
       return
     end
 
-    safe_slug = Pathname.new(slug).cleanpath.to_s
-    safe_filename = Pathname.new(filename).cleanpath.to_s
-
-    file_path = Rails.root.join("persistent_disk", "cats", safe_slug, "photos", safe_filename)
-
-    unless file_path.realpath.to_s.start_with?(Rails.root.join("persistent_disk", "cats").to_s)
-      render plain: "Invalid file path", status: :bad_request
+    folders = registered_cat_list
+    folder = folders.find { |f| f == slug }
+    unless folder
+      render plain: "Cat not found", status: :not_found
       return
     end
+
+    photos = photos_list_for_cat(folder)
+    photo = photos.find { |p| p == filename }
+    unless photo
+      render plain: "Photo not found", status: :not_found
+      return
+    end
+
+    file_path = Rails.root.join("persistent_disk", "cats", folder, "photos", photo)
 
     if File.exist?(file_path)
       send_file file_path, disposition: "inline"
     else
       render plain: "File not found", status: :not_found
     end
+  end
+
+  private
+
+  def registered_cat_list
+    folders = $redis.get("registered_cat_folders")
+    if folders
+      return JSON.parse(folders)
+    end
+
+    base_path = Rails.root.join("persistent_disk", "cats")
+    folders = Dir.entries(base_path).select do |entry|
+      File.directory?(File.join(base_path, entry)) && !(entry == "." || entry == "..")
+    end
+
+    $redis.setex("registered_cat_folders", 10.minutes, folders.to_json)
+    folders
+  end
+
+  def photos_list_for_cat(cat)
+    photos_list = $redis.get("photos_list_#{cat}")
+    if photos_list
+      return JSON.parse(photos_list)
+    end
+
+    base_path = Rails.root.join("persistent_disk", "cats", cat, "photos")
+    photos_list = Dir.entries(base_path).select do |entry|
+      entry.match?(/\A[a-zA-Z0-9_-]+\.(jpg|webp)\z/) # REMOVE jpg LATER
+    end
+
+    $redis.setex("photos_list_#{cat}", 10.minutes, photos_list.to_json)
+    photos_list
   end
 end
